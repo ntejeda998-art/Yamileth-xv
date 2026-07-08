4886962958143bf34f8d1a3d8ac43ba60531d07c
ntejeda998-art <ntejeda998@gmail.com>
2026-07-08 00:02:47 -0600

    Agregar JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const musica = document.getElementById("musica");
 
+window.addEventListener("click", () => {
+    if (musica.paused) {
+        musica.play();
+    }
+}, { once: true });
+
+function confirmarAsistencia() {
+    const mensaje = "¡Hola! Confirmo mi asistencia a los XV de Yamileth. ✨";
+    const numero = "525632462685";
+    window.open(`https://wa.me/${numero}?text=${encodeURIComponent(mensaje)}`, "_blank");
+}
